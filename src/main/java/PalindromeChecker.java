62d41791dd6d6b92faa4d0bc1ae1622dbb22c013
Alec Tunbridge <alec.tunbridge@gmail.com>
2016-02-05 14:45:39 +0000

    first working version

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -1,7 +1,22 @@
 
 public class PalindromeChecker {
 
-    public boolean check(String word) {
-        return false;
+    public boolean isPalindrome(String word) {
+        if (word == null){
+            return false;
+        }
+
+        String sanitized = word.toLowerCase().replaceAll("\\W", "");
+
+        int midPoint = sanitized.length() / 2 -1;
+        int x = sanitized.length()-1;
+        for(int i=0; i<=midPoint; i++){
+            if(sanitized.charAt(i) != sanitized.charAt(x)){
+                return false;
+            }
+            x -= 1;
+        }
+
+        return true;
     }
 }
--- src/test/java/PalindromeCheckerTest.java
+++ src/test/java/PalindromeCheckerTest.java
@@ -13,7 +13,57 @@ public class PalindromeCheckerTest {
     }
 
     @Test
-    public void test() {
-        assertTrue(palindromeChecker.check(null));
+    public void checkNullInput() {
+        assertFalse(palindromeChecker.isPalindrome(null));
+    }
+
+    @Test
+    public void checkSingleLetter(){
+        assertTrue(palindromeChecker.isPalindrome("a"));
+    }
+
+    @Test
+    public void checkTwoDifferentLetters(){
+        assertFalse(palindromeChecker.isPalindrome("ab"));
+    }
+
+    @Test
+    public void checkTwoSameLetters(){
+        assertTrue(palindromeChecker.isPalindrome("aa"));
+    }
+
+    @Test
+    public void checkAThreeLetterValidPalindrome() throws Exception {
+        assertTrue(palindromeChecker.isPalindrome("aba"));
+    }
+
+    @Test
+    public void checkAThreeLetterInvalidPalindrome() throws Exception {
+        assertFalse(palindromeChecker.isPalindrome("abb"));
+    }
+
+    @Test
+    public void checkAFourLetterInvalidPalindrome() throws Exception {
+        assertFalse(palindromeChecker.isPalindrome("abca"));
+    }
+
+    @Test
+    public void checkAFourLetterValidPalindrome(){
+        assertTrue(palindromeChecker.isPalindrome("boob"));
+    }
+
+    @Test
+    public void checkAFiveLetterValidPalindrome(){
+        assertTrue(palindromeChecker.isPalindrome("level"));
+    }
+
+    @Test
+     public void checkAPhraseIsAValidPalindromicPhrase(){
+        assertTrue(palindromeChecker.isPalindrome("race car"));
+    }
+
+    @Test
+    public void checkAValidPalindromicSentence(){
+        assertTrue(palindromeChecker.isPalindrome("Are we not drawn onward, we few, drawn onward to new era?"));
     }
 }
