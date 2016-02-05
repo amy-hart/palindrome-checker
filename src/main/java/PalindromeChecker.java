438a31ad057f8875720f2877f117c32d9f2499bb
Alec Tunbridge <alec.tunbridge@gmail.com>
2016-02-05 15:12:44 +0000

    Cheating is fun

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -2,21 +2,10 @@
 public class PalindromeChecker {
 
     public boolean isPalindrome(String word) {
-        if (word == null){
+        if(word==null){
             return false;
         }
-
-        String sanitized = word.toLowerCase().replaceAll("\\W", "");
-
-        int midPoint = sanitized.length() / 2 -1;
-        int x = sanitized.length()-1;
-        for(int i=0; i<=midPoint; i++){
-            if(sanitized.charAt(i) != sanitized.charAt(x)){
-                return false;
-            }
-            x -= 1;
-        }
-
-        return true;
+        String sanitizedWord = word.toLowerCase().replaceAll("\\W","");
+        return new StringBuffer(sanitizedWord).reverse().toString().equals(sanitizedWord);
     }
 }
